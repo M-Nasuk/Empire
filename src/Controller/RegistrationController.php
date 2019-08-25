9976d3fec6b29754dc4429a1543aa395925e1ff5
M-Nasuk <m.kusanagi@gmx.com>
2019-08-26 00:13:58 +0200

    Add mail event listener

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Event\RegisterEvent;
+use App\EventListener\RegisterListener;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,17 +13,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param UserAuthenticator $authenticator
+     * @param EventDispatcherInterface $dispatcher
+     * @param RegisterListener $listener
+     * @return Response
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthenticator $authenticator, EventDispatcherInterface $dispatcher, RegisterListener $listener): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+
+        $dispatcher->addListener(RegisterEvent::NAME, [$listener, 'sendMailToUser']);
+
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
@@ -37,6 +50,8 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
+            $event = new RegisterEvent($user);
+            $dispatcher->dispatch($event, RegisterEvent::NAME);
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
---
+++ src/EventListener/RegisterListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventListener;
+
+use Swift_Mailer;
+use Swift_Message;
+
+class RegisterListener
+{
+    public $mailer;
+    private $adminMail;
+
+    /**
+     * RegisterListener constructor.
+     * @param Swift_Mailer $mailer
+     * @param $adminMail
+     */
+    public function __construct(Swift_Mailer $mailer, $adminMail)
+    {
+        $this->mailer = $mailer;
+        $this->adminMail = $adminMail;
+    }
+
+    public function sendMailToUser()
+    {
+        $message = (new Swift_Message('Hello'))
+            ->setFrom('ok@test.fr')
+            ->setTo($this->adminMail)
+            ->setBody('Here is the message itself');
+
+        $this->mailer->send($message);
+    }
+}
